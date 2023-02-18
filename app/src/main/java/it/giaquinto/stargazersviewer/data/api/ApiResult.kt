c520d9392166b408ad6739f76045efadef516b4a
Alessandro Giaquinto <alessandro.giaquinto87@gmail.com>
2023-02-18 12:54:37 +0100

    Update ApiResult.kt
    Edit visibility for security problem

--- app/src/main/java/it/giaquinto/stargazersviewer/data/api/ApiResult.kt
+++ app/src/main/java/it/giaquinto/stargazersviewer/data/api/ApiResult.kt
@@ -2,14 +2,14 @@ package it.giaquinto.stargazersviewer.data.api
 
 sealed class ApiResult<out T> (val status: ApiStatus, val data: T?, val message:String?, statusCode: Int?) {
 
-    data class Success<out R>(val _data: R?): ApiResult<R>(
+    data class Success<out R>(private val _data: R?): ApiResult<R>(
         status = ApiStatus.SUCCESS,
         data = _data,
         message = null,
         statusCode = 200
     )
 
-    data class Error(val exception: String, val _statusCode: Int?): ApiResult<Nothing>(
+    data class Error(private val exception: String, private val _statusCode: Int?): ApiResult<Nothing>(
         status = ApiStatus.ERROR,
         data = null,
         message = exception,
